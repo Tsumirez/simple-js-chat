5f18b47183d16ad3b5fddd1eec8f8300d637e1ec
Zef <tsumirez@zoho.com>
2021-03-17 23:21:20 +0100

    simple websockets and node.js based chat application.
    Status: incomplete;
    
    Current functionality:
    - can select a nick and join the chat room
    - can type messages
    - will display messages and user list for all participants.
    
    Missing parts:
    - making log in mandatory
    - private chats
    - modularization of code
    - additional features (such as client chat history, styling, adding pictures to chat etc)

---
+++ index.js
@@ -0,0 +1,33 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const users = new Set();
+
+app.get('/', (req,res) => {
+    //res.send('<h1>Hello World</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+
+    socket.on('chat message', msg => {
+        io.emit('chat message', socket.user + ': ' + msg);
+    })
+
+    socket.on('add user', user => {
+        users.add(user);
+        console.log(users);
+        socket.user = user;
+        io.emit('chat message', `${user} joined the chat!`)
+        io.emit('user list', {users: [...users]})
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+})
+
+http.listen(3000,() => {
+    console.log('listening on port 3000');
+})
