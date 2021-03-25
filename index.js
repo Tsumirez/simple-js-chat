9cecc25cb8689bddca35c020591ab4b299c272a5
Zef <tsumirez@zoho.com>
2021-03-25 15:45:29 +0100

    Added private messaging functionality (whispers)
    Made some major changes to how messaging works to get this going but it works
    Messages now send objects with data rather than plain strings.
    Users are now a Map where names are keys and their socket ids are values.
    Clicking a name on the right-side user list sets chat to private mode with that person. Only they will
    see the message you sent which is reflected in your nick input field, with "to (your whispers target)"
    
    Things that still need to be done:
    Leaving the private chat mode and going back to main chat.
    Styling to distinguish incoming messages from the ones you sent in private convo.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-const users = new Set();
+const users = new Map();
 
 app.use(express.static('public'));
 // app.get('/', (req,res) => {
@@ -22,16 +22,23 @@ io.on('connection', (socket) => {
             socket.emit('nick taken',user);
             return;
         }
-        users.add(user);
+        users.set(user,socket.id);
         socket.user = user;
-        io.emit('chat message', `${user} joined the chat!`)
-        io.emit('user add', {users: [...users]})
+        io.emit('message', {msg:`${user} joined the chat!`})
+        io.emit('user add', {users: [...users.keys()]})
     })
 
     socket.on('disconnect', () => {
         users.delete(socket.user);
-        io.emit('user left', {users: [...users]})
-        io.emit('chat message', `${socket.user} has left the chat`)
+        io.emit('user left', {users: [...users.keys()]})
+        io.emit('message', {msg: `${socket.user} has left the chat`})
+    })
+
+    socket.on('message',({to, msg}=data) => {
+        console.log(`message ${msg} received`)
+        target = to?socket.to(users.get(to)):io;
+        target.emit('message', {to:to, msg:`${socket.user}: ${msg}`});
+        if (to) socket.emit('message', {to:to, msg: `to ${to}: ${msg}`})
     })
 })
 
